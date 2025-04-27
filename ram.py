8c9a92eeb6672c9ae488bcf26dc040bbcd8388e9
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-04-27 16:26:03 +0200

    added safe RAM class with configurable base address

--- ram.py
+++ ram.py
@@ -85,6 +85,10 @@ class RAM:
             raise MemoryAccessError(f"Exceeded maximum length while reading C string at 0x{addr:08X}")
         return memory_slice[:nul_index].decode('utf-8', errors='replace')
 
+
+# For performance reasons, we provide multiple implementations of the emulated RAM
+# rather than a unified one with several options.
+
 # Safe RAM class: checks all addresses
 class SafeRAM:
     def __init__(self, size=1024*1024, init=None, logger=None):
@@ -138,6 +142,78 @@ class SafeRAM:
         self.memory[addr:addr+len(binary)] = binary
 
     def load_cstring(self, addr, max_len=1024):
+        if addr < 0 or addr >= self.size:
+            raise MemoryAccessError(f"Invalid start address reading C string: 0x{addr:08X}")
+        end = min(addr + max_len, self.size)
+        memory_slice = self.memory[addr:end]
+        nul_index = memory_slice.find(0)
+        if nul_index == -1:
+            raise MemoryAccessError(f"Exceeded maximum length while reading C string at 0x{addr:08X}")
+        return memory_slice[:nul_index].decode('utf-8', errors='replace')
+
+# Safe RAM class + optional base address
+class SafeRAMOffset:
+    def __init__(self, size=1024*1024, base_addr=0, init=None, logger=None):
+        self.memory = bytearray(size)
+        self.size = size
+        self.base_addr = base_addr
+        self.logger = logger
+        if init is not None and init != 'zero':
+            initialize_ram(self, init)
+
+    def check(self, addr, n):
+        if addr < 0 or addr + n > self.size:
+            raise MemoryAccessError(f"Access out of bounds: 0x{self.base_addr+addr:08X} (+{n})")
+
+    def load_byte(self, addr, signed=True):
+        addr -= self.base_addr
+        self.check(addr, 1)
+        val = self.memory[addr]
+        return val if not signed or val < 0x80 else val - 0x100
+ 
+    def load_half(self, addr, signed=True):
+        addr -= self.base_addr
+        self.check(addr, 2)
+        val = self.memory[addr] | (self.memory[addr+1] << 8)
+        return val if not signed or val < 0x8000 else val - 0x10000
+
+    def load_word(self, addr):  # always unsigned (performance)
+        addr -= self.base_addr
+        self.check(addr, 4)
+        return self.memory[addr] | (self.memory[addr+1] << 8) | self.memory[addr+2] << 16 | self.memory[addr+3] << 24
+
+    def store_byte(self, addr, value):
+        addr -= self.base_addr
+        self.check(addr, 1)
+        self.memory[addr] = value & 0xFF
+
+    def store_half(self, addr, value):
+        addr -= self.base_addr
+        value &= 0xFFFF  # make it unsigned
+        self.memory[addr] = value & 0xFF
+        self.memory[addr+1] = (value >> 8) & 0xFF
+
+    def store_word(self, addr, value):
+        addr -= self.base_addr
+        self.check(addr, 4)
+        value &= 0xFFFFFFFF  # make it unsigned
+        self.memory[addr] = value & 0xFF
+        self.memory[addr+1] = (value >> 8) & 0xFF
+        self.memory[addr+2] = (value >> 16) & 0xFF
+        self.memory[addr+3] = (value >> 24) & 0xFF
+
+    def load_binary(self, addr, n):
+        addr -= self.base_addr
+        self.check(addr, n)
+        return self.memory[addr:addr+n]
+
+    def store_binary(self, addr, binary):
+        addr -= self.base_addr
+        self.check(addr, n=len(binary))
+        self.memory[addr:addr+len(binary)] = binary
+
+    def load_cstring(self, addr, max_len=1024):
+        addr -= self.base_addr
         if addr < 0 or addr >= self.size:
             raise MemoryAccessError(f"Invalid start address reading C string: 0x{addr:08X}")
         end = min(addr + max_len, self.size)
