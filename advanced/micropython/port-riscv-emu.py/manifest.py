dd1a63a73c82661efa28d1757094bd6612ffbf79
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-11-25 08:25:48 +0100

    MicroPython: simplified handling of frozen startup script

--- advanced/micropython/port-riscv-emu.py/manifest.py
+++ advanced/micropython/port-riscv-emu.py/manifest.py
@@ -1,6 +1,6 @@
 # Manifest for freezing Python scripts into firmware
 # This file defines which Python modules should be compiled and frozen into the firmware
 
-# Freeze startup.py (the default embedded script)
-# To freeze a different script, modify this file or specify FROZEN_SCRIPT in your Makefile
+# Freeze startup.py from the port directory
+# For HEADLESS and UART modes, startup.py will be automatically executed on boot
 freeze("$(PORT_DIR)", "startup.py")
